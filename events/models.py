44b7c87bca78fef73cf8c1cf121d590b7d9d0d85
unknown <ualekseeva263@gmail.com>
2025-10-13 20:38:49 +0300

    j

--- events/models.py
+++ events/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
+# description, creator
 
-# Create your models here.
+class Event(models.Model):
+    title = models.CharField(max_length=100)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    description = models.TextField()
+    creation_time = models.DateTimeField(auto_now_add=True)
