093f7cf110377cbbfc29a930abad1e67fe2d41c2
olaa321 <olaa15444@gmail.com>
2025-10-13 20:57:13 +0300

    forum

---
+++ events/forum/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+#Тема опитування
+class Topic(models.Model):
+    title = models.CharField(max_length=200)
+    descripton = models.TextField()
+    creator = models.ForeignKey(User, on_delete=models.CASCADE, related_name="creator")
+    created_time = models.DateTimeField()
+
+#Пост користувача в якомусь опитуванні
+class Post(models.Model):
+    topic = models.CharField(max_length=200)
+    content = models.TextField()
+    creator = models.ForeignKey(User, on_delete=models.CASCADE, related_name="creator")
+    created_time = models.DateTimeField()
