8dca6e569f7191c34cb4e973b32580180044b4c4
yula334 <165807001+yula334@users.noreply.github.com>
2025-10-13 14:17:17 +0300

    Update models

--- events/models.py
+++ events/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+# description, creator
+
+class h():
+    title = models.CharField(max_length=100)
+    user = models.ForeignKey()
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    description = models.TextField()
+    creation_time = models.DateTimeField(auto_now_add=True)
