3fa91483233a60e76b4cb2ab3d5d8c5bb914bae2
olaa321 <olaa15444@gmail.com>
2025-10-19 21:28:51 +0300

    forum_templates

--- forum/admin.py
+++ forum/admin.py
@@ -1,3 +1,3 @@
 from django.contrib import admin
+from .models import Post, Topic
 
-# Register your models here.
--- forum/views.py
+++ forum/views.py
@@ -1,31 +1,43 @@
 from django.shortcuts import render
+from django.views.generic import DeleteView, UpdateView, CreateView, ListView, DetailView
+from .models import Post, Topic
+from django.urls import reverse_lazy
 
 # Список тем
-class TopicListView():
+class TopicListView(ListView):
+    model = Topic
 
     template_name = "tasks/topic_list.html"
+    context_object_name = "topics"
 
 # Деталі конкретної теми
-class TopicDetailView():
-
+class TopicDetailView(DetailView):
+    model = Topic
     template_name = "tasks/topic_details.html"
+    context_object_name = "topic"
 
 # Створення нової теми
-class TopicCreateView():
+class TopicCreateView(CreateView):
+    model = Topic
     
     template_name = "tasks/topic_create.html"
 
 # Додавання постів до теми
-class PostCreateView():
+class PostCreateView(CreateView):
+    model = Post
 
     template_name = "tasks/post_create.html"
 
 # Редагування постів до теми
-class PostUpdateView():
+class PostUpdateView(UpdateView):
+    model = Post
 
+    success_url = reverse_lazy("topic-list")
     template_name = "tasks/post_update.html"
 
 # Видалення постів до теми
-class PostDeleteView():
-
+class PostDeleteView(DeleteView):
+    model = Post
     template_name = "tasks/post_delete.html"
+    success_url = reverse_lazy("topic-list")
+    context_object_name = "post"
