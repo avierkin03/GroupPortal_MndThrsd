2a1c45c10ee6dc0f5cf0e9bff41f570901c7ecac
unknown <ualekseeva263@gmail.com>
2025-10-20 04:36:32 +0300

    f

--- events/urls.py
+++ events/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    
+    path("", views.EventListView.as_view(), name="event_list"),
+    path("", views.EventCreateView.as_view(), name="event_create"),
+    path("", views.EventUpdateView.as_view(), name="event_update"),
+    path("", views.EventDeleteView.as_view(), name="event_delete"),
 ]
--- events/views.py
+++ events/views.py
@@ -1,17 +1,24 @@
 from django.shortcuts import render
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from .models import Event
 
 # Список подій
-class EventListView():
-    ...
+class EventListView(ListView):
+    model = Event
+    template_name = "events/events_list.html"
+    context_object_name = "events"
 
 # Створення нової події
-class EventCreateView():
-    ...
+class EventCreateView(CreateView):
+    model = Event
+    template_name = "events/event_create.html"
 
 # Редагування існуючої події
-class EventUpdateView():
-    ...
+class EventUpdateView(UpdateView):
+    model = Event
+    template_name = "events/event_update.html"
 
 # Видалення існуючої події
-class EventDeleteView():
-    ...
+class EventDeleteView(DeleteView):
+    model = Event
+    template_name = "events/event_delete.html"
