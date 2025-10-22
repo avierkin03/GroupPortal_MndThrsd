38d9fd5955a79b6ddcc7b0d1ff9a9f6e07aabef0
avierkin03 <avierkin03@gmail.com>
2025-10-22 16:36:10 +0300

    fixed some bugs

--- forum/models.py
+++ forum/models.py
@@ -5,12 +5,12 @@ from django.contrib.auth.models import User
 class Topic(models.Model):
     title = models.CharField(max_length=200)
     descripton = models.TextField()
-    creator = models.ForeignKey(User, on_delete=models.CASCADE, related_name="creator")
+    creator = models.ForeignKey(User, on_delete=models.CASCADE, related_name="topics")
     created_time = models.DateTimeField()
 
 #Пост користувача в якомусь опитуванні
 class Post(models.Model):
     topic = models.CharField(max_length=200)
     content = models.TextField()
-    creator = models.ForeignKey(User, on_delete=models.CASCADE, related_name="creator")
+    creator = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
     created_time = models.DateTimeField()
