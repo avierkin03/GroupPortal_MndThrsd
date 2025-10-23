52fee99df3ec30e5b0bba336ad049fb6c2f11d86
unknown <ualekseeva263@gmail.com>
2025-10-23 18:31:22 +0300

    g

--- events/urls.py
+++ events/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.EventListView.as_view(), name="event_list"),
-    path("", views.EventCreateView.as_view(), name="event_create"),
-    path("", views.EventUpdateView.as_view(), name="event_update"),
-    path("", views.EventDeleteView.as_view(), name="event_delete"),
+    path("event_create/", views.EventCreateView.as_view(), name="event_create"),
+    path("event_update/<int:pk>", views.EventUpdateView.as_view(), name="event_update"),
+    path("event_delete/<int:pk>", views.EventDeleteView.as_view(), name="event_delete"),
 ]
--- events/views.py
+++ events/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from .models import Event
+from .forms import EventForm
+from django.urls import reverse_lazy
 
 # Список подій
 class EventListView(ListView):
@@ -12,13 +14,18 @@ class EventListView(ListView):
 class EventCreateView(CreateView):
     model = Event
     template_name = "events/event_create.html"
+    form_class = EventForm
+    success_url = reverse_lazy("event_list")
 
 # Редагування існуючої події
 class EventUpdateView(UpdateView):
     model = Event
     template_name = "events/event_update.html"
+    form_class = EventForm
+    success_url = reverse_lazy("event_list")
 
 # Видалення існуючої події
 class EventDeleteView(DeleteView):
     model = Event
     template_name = "events/event_delete.html"
+    success_url = reverse_lazy("event_list")
